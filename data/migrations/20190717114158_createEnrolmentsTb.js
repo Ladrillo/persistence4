f1b13d21c8b442f5e2a07c593e96653627dacaca
Gabriel Cabrejas <gabriel@cabrejas.net>
2019-07-18 05:39:21 -0400

    wip

--- data/migrations/20190717114158_createEnrolmentsTb.js
+++ data/migrations/20190717114158_createEnrolmentsTb.js
@@ -1,8 +1,21 @@
-
-exports.up = function (knex) {
-
+exports.up = function(knex) {
+  return knex.schema.createTable("enrollments", table => {
+    table.increments();
+    table
+      .integer("user_id")
+      .unsigned()
+      .notNullable()
+      .references("id")
+      .inTable("users");
+    table
+      .integer("course_id")
+      .unsigned()
+      .notNullable()
+      .references("id")
+      .inTable("courses");
+  });
 };
 
-exports.down = function (knex) {
-
+exports.down = function(knex) {
+  return knex.schema.dropTableIfExists("enrollments");
 };
